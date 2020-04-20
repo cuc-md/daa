d5ee178ff3ce9b49b77550fd522eff01325deb68
Alexei Șerșun <alex.sersun@yahoo.com>
2020-04-20 17:23:49 +0300

    Add tests for user registration.

--- gateway/app/controllers/users/registrations_controller.rb
+++ gateway/app/controllers/users/registrations_controller.rb
@@ -1,15 +1,22 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
   respond_to :json
 
   def create
-    super
-
+    build_resource(sign_up_params)
+    resource.save
     if resource.errors.any?
       render json: { error: { details: resource.errors } }
     else
-      render json: resource
+      render json: { data: resource.slice("name", "email") }
     end
+  end
+
+  protected
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
