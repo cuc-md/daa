f17981409cebdccc5914c8f16a9d33245457652b
Ksenia <xeniazvercova@gmail.com>
2020-04-24 18:05:52 +0300

    add base club style

--- web_app/src/components/Clubs/Clubs.js
+++ web_app/src/components/Clubs/Clubs.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Club from './Club';
 import './Clubs.css';
 
 class Clubs extends Component {
@@ -11,7 +12,31 @@ class Clubs extends Component {
     }
 
     render() {
-        return <div className="main">Clubs</div>
+        return <div className="main">
+            <div>
+                <div className="clubsTableHead">
+                    <div className="clubNumber"/>
+                    <div className="clubName">
+                        Club
+                    </div>
+                </div>
+                <Club keyItem={1}
+                      divItemId="id1"
+                      divItemIdToggler="#id1"
+                      clubNumber={1}
+                      clubName="Club1"/>
+                <Club keyItem={2}
+                      divItemId="id2"
+                      divItemIdToggler="#id2"
+                      clubNumber={2}
+                      clubName="Club2"/>
+                <Club keyItem={3}
+                      divItemId="id3"
+                      divItemIdToggler="#id3"
+                      clubNumber={3}
+                      clubName="Club3"/>
+            </div>
+        </div>
     }
 }
 
