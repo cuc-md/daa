b74c081d54835bd79be9e4251b84de2e7d478ec6
Alexei Șerșun <alex.sersun@yahoo.com>
2020-04-25 15:26:51 +0300

    Add roles to users.

--- gateway/app/models/user.rb
+++ gateway/app/models/user.rb
@@ -3,4 +3,34 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable, :recoverable, :validatable,
     :jwt_authenticatable, jwt_revocation_strategy: self
+
+  ROLES = %w(manage_events manage_all_events)
+  ROLES.each do |role|
+    const_set(role.upcase, role)
+  end
+
+  validate :roles_validation
+
+  def add_role(new_role)
+    return unless ROLES.include?(new_role)
+    self.roles += [new_role]
+  end
+
+  def add_roles(roles)
+    roles.each { |role| add_role(role) }
+  end
+
+  def remove_role(role)
+    return unless ROLES.include?(role)
+    self.roles -= [role]
+  end
+
+  def remove_roles(roles)
+    roles.each { |role| remove_role(role) }
+  end
+
+  def roles_validation
+    unknown_roles = roles - ROLES
+    errors.add(:roles, "Unknown roles: #{unknown_roles.join(', ')}") if unknown_roles.size > 0
+  end
 end
