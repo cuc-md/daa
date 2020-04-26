b4e8ff619d81f3213817ce5d71ddadf22a0bed7d
Alexei Șerșun <alex.sersun@yahoo.com>
2020-04-26 13:31:19 +0300

    Add lograge logger.

--- gateway/config/environments/development.rb
+++ gateway/config/environments/development.rb
@@ -17,4 +17,5 @@ Rails.application.configure do
   config.active_support.deprecation = :log
   config.active_record.migration_error = :page_load
   config.active_record.verbose_query_logs = true
+  config.log_level = :debug
 end
