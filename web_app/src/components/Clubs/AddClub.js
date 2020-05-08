66a98570777080fc53e8074341efc830799b371a
Ksenia <xeniazvercova@gmail.com>
2020-05-08 18:29:30 +0300

    add datepicker

--- web_app/src/components/Clubs/AddClub.js
+++ web_app/src/components/Clubs/AddClub.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import DatePicker from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.css';
 import '../Utils/Form/Form.css';
 
 class AddClub extends Component {
@@ -8,18 +10,22 @@ class AddClub extends Component {
         this.state = {
             name: '',
             city: '',
-            foundedOn: '',
+            foundedOn: new Date(),
             description: '',
             representative: '',
             phone: '',
-            email: ''
+            email: '',
         };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleDateChange = this.handleDateChange.bind(this);
     }
 
     handleChange(e) {
-        this.setState({
-            [e.target.name]: e.target.value
-        });
+        this.setState({[e.target.name]: e.target.value});
+    };
+
+    handleDateChange(date) {
+        this.setState({foundedOn: date});
     };
 
     // handleSubmit(e) {
@@ -37,58 +43,56 @@ class AddClub extends Component {
                 <br/>
                 <input
                     type="name"
-                    name='name'
-                    placeholder='Club name'
+                    name="name"
+                    placeholder="Club name"
                     value={this.state.name}
                     onChange={this.handleChange}
                     required
                 />
                 <input
                     type="name"
-                    name='city'
-                    placeholder='City'
+                    name="city"
+                    placeholder="City"
                     value={this.state.city}
                     onChange={this.handleChange}
                     required
                 />
-                {/*TODO add datepicker*/}
-                <input
-                    type='name'
-                    name='foundedOn'
-                    placeholder='Founded on'
-                    value={this.state.foundedOn}
-                    onChange={this.handleChange}
-                    required
+                <DatePicker
+                    selected={this.state.foundedOn}
+                    onChange={this.handleDateChange}
+                    className="inputDatepicker"
+                    placeholder="Representative"
+                    dateFormat="yyyy-MM-dd"
                 />
                 <input
-                    type='name'
-                    name='description'
-                    placeholder='Description'
+                    type="name"
+                    name="description"
+                    placeholder="Description"
                     value={this.state.description}
                     onChange={this.handleChange}
                     required
                 />
                 <input
-                    type='name'
-                    name='representative'
-                    placeholder='Representative'
+                    type="name"
+                    name="representative"
+                    placeholder="Representative"
                     value={this.state.representative}
                     onChange={this.handleChange}
                     required
                 />
                 {/*TODO check phone*/}
                 <input
-                    type='name'
-                    name='phone'
-                    placeholder='Phone'
+                    type="name"
+                    name="phone"
+                    placeholder="Phone"
                     value={this.state.phone}
                     onChange={this.handleChange}
                     required
                 />
                 <input
-                    type='email'
-                    name='email'
-                    placeholder='Email'
+                    type="email"
+                    name="email"
+                    placeholder="Email"
                     value={this.state.email}
                     onChange={this.handleChange}
                     required
