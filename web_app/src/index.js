4a2defea2afab5f438e9b079bc0b1bb61dc60d9a
Ksenia <xeniazvercova@gmail.com>
2020-05-13 20:02:04 +0300

    add redux persist

--- web_app/src/index.js
+++ web_app/src/index.js
@@ -5,15 +5,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {PersistGate} from 'redux-persist/lib/integration/react';
 import storeConfiguration from './store/storeConfigutarion';
 
-const {store} = storeConfiguration();
+const {store, persistor} = storeConfiguration();
 
 ReactDOM.render(
     <Provider store={store}>
-        <App/>
-    </Provider>,
-    document.getElementById('root')
-);
-
+        <PersistGate persistor={persistor} loading={null}>
+            <App/>
+        </PersistGate>
+    </Provider>, document.getElementById('root'));
 serviceWorker.register();
