7de04d57d45b3c4faaa916a401768a1a08cbd95f
Ksenia <xeniazvercova@gmail.com>
2020-05-17 21:13:24 +0300

    register team for event

---
+++ web_app/src/components/Teams/RegisterTeamForEvent.js
@@ -0,0 +1,97 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import toaster from 'toasted-notes';
+import {PopupboxManager} from 'react-popupbox';
+import '../Utils/Toaster/Toaster.css';
+import '../Utils/Form/Form.css';
+
+class RegisterTeamForEvent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            captain: '',
+            phone: '',
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({[e.target.name]: e.target.value});
+    };
+
+    handleSubmit(e) {
+        e.preventDefault();
+        fetch('/api/v1/events/' + this.props.eventId + '/registration ', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': this.props.token
+            },
+            body: JSON.stringify(this.state)
+        }).then(response => {
+            if (!response.ok) {
+                toaster.notify("Error", {
+                    duration: 3000,
+                    position: "bottom"
+                });
+            } else {
+                toaster.notify("Team was successfully registered", {
+                    duration: 3000,
+                    position: "bottom"
+                });
+                PopupboxManager.close();
+                return response.json();
+            }
+        })
+    };
+
+    render() {
+        return <div>
+            <form className="form"
+                  onSubmit={this.handleSubmit}>
+                <h3 className="formText">
+                    Register team for event
+                </h3>
+                <br/>
+                <input
+                    type="name"
+                    name="name"
+                    placeholder="Team name"
+                    value={this.state.name}
+                    onChange={this.handleChange}
+                    required
+                />
+                <input
+                    type="name"
+                    name="captain"
+                    placeholder="Captain"
+                    value={this.state.captain}
+                    onChange={this.handleChange}
+                    required
+                />
+                <input
+                    type="tel"
+                    name="phone"
+                    placeholder="Phone"
+                    pattern="^\+?\d{0,13}"
+                    value={this.state.phone}
+                    onChange={this.handleChange}
+                    required
+                />
+                <input type='submit'
+                       className="formButton textFontStyle16"/>
+            </form>
+        </div>
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        token: state.token
+    }
+};
+
+export default connect(mapStateToProps, null)(RegisterTeamForEvent);
