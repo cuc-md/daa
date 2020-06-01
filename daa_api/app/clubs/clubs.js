2560379aab6ddec674d26cea4b7740a2c5abe855
Nina Jechiu <nina.jechiu@indrivo.com>
2020-06-01 16:12:58 +0300

    Add db for API for Clubs,Teams, Events

---
+++ daa_api/app/clubs/clubs.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const ClubsSchema = mongoose.Schema({
+    id_club: Number,
+    club_name: String,
+    city: String,
+    address: String,
+    description: String,
+    founded_on: Date,
+    active_teams: Number,
+    total_teams: Number,
+});
+
+module.exports = mongoose.model('DAA', ClubsSchema);
