751c54118b7b53a3ccfeb826da56f2b783ff1b2d
Alexei Șerșun <alex.sersun@yahoo.com>
2020-06-01 23:18:34 +0300

    Dockerize events.

---
+++ events/app/teams/teams.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const TeamsSchema = mongoose.Schema({
+    id_team: Number,
+    team_name: String,
+    captain: String,
+    phone: Number
+});
+
+module.exports = mongoose.model('Team', TeamsSchema);
